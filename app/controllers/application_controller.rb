a744bf3117ffc977409eb996aa5a4e239e185fbd
Donovan Jones <donovanljones94@gmail.com>
2019-12-03 17:59:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,57 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    redirect '/recipes'
+  end
+  
+  get '/recipes' do
+    @recipes = Recipe.all 
+    erb :index
+  end
 
-  # code actions here!
+  get '/recipes/new' do 
+    erb :new 
+  end
+  
+  post '/recipes' do 
+    @name = params[:name]
+    @ingredients = params[:ingredients]
+    @cook_time = params[:cook_time]
+    recipe = Recipe.new(:name => @name, :ingredients => @ingredients, :cook_time => @cook_time)
+    recipe.save
+    redirect "/recipes/#{recipe.id}"
+  end
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @name = @recipe.name
+    @ingredients = @recipe.ingredients
+    @cook_time=@recipe.cook_time
+    erb :recipe
+  end
+  
+  delete '/recipes/:id' do 
+    Recipe.find(params[:id]).destroy
+    redirect '/recipes'
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+      recipe.update(
+        name: params[:name],
+        ingredients: params[:ingredients],
+        cook_time: params[:cook_time]
+      )
+
+      redirect "/recipes/#{recipe.id}"
+
+  end
 
 end
